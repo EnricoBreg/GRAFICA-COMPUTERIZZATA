821e86776f2541c8004093ced6e1d701d42b336c
Enrico <enrico.bregoli@edu.unife.it>
2021-02-03 09:14:52 +0100

    prova cubetto

---
+++ cuboRubik/fragmentShader.glsl
@@ -0,0 +1,13 @@
+#version 430 core
+
+smooth in vec4 colorsExport;
+
+layout(depth_less) out float gl_FragDepth;
+//out float gl_FragColor;
+
+out vec4 colorsOut;
+
+void main(void) {
+   colorsOut = colorsExport;
+   gl_FragDepth = gl_FragCoord.z;
+}
